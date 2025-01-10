45e6eab7e1ed57e10b2a36c09edd2de2956e60e5
zhiyong <yuzhiyongcn@qq.com>
2025-01-10 14:33:28 +0800

    fix: cannot login

--- easytrader/clienttrader/ths519_clienttrader.py
+++ easytrader/clienttrader/ths519_clienttrader.py
@@ -20,6 +20,7 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
     同花顺v5.19.15.133的实现(xiadan.exe)
     和5.18的界面相比，发生了变化，需要重新子类化
     """
+
     grid_strategy = grid_strategies.Copy
 
     @property
@@ -49,7 +50,7 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
             self._app = pywinauto.Application().connect(
                 path=self._run_exe_path(exe_path), timeout=1
             )
-            logger.info("尝试连接到原有进程[%s]成功",exe_path)
+            logger.info("尝试连接到原有进程[%s]成功", exe_path)
 
         # pylint: disable=broad-except
         except Exception:
@@ -60,8 +61,7 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
             logger.info("启动软件完毕，等待登录窗口出现")
             while True:
                 try:
-                    login_window_handle = pywinauto.findwindows.find_window(class_name='#32770', found_index=1)
-                    login_window = self._app.window(handle=login_window_handle)
+                    login_window = self._app.top_window()
                     login_window.Edit1.wait("ready")
                     break
                 except WindowNotFoundError:
@@ -78,12 +78,12 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
             logger.debug("密码控件ID：%s", login_window.Edit2.control_id())
 
             # 处理验证码
-            verify_code_result = self._handle_verify_code_input(login_window, login_window_handle)
+            verify_code_result = self._handle_verify_code_input(login_window)
             if verify_code_result == 1:  # 没有验证码控件
                 # 直接点击登录
-                self._app.window(handle=login_window_handle)["登录"].click()
+                self._app.top_window()["登录"].click()
                 # 等待登录窗口消失
-                self._app.window(handle=login_window_handle).wait_not("exists", 5)
+                self._app.top_window().wait_not("exists", 5)
             elif verify_code_result == 2:  # 验证码输入失败
                 logger.error("验证码输入失败，登录终止")
                 return False
@@ -96,13 +96,13 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
         logger.debug("主窗口：%r", self._app.window(title="网上股票交易系统5.0"))
         self._main = self._app.window(title="网上股票交易系统5.0")
 
-    def _handle_verify_code_input(self, login_window, login_window_handle):
+    def _handle_verify_code_input(self, login_window):
         """处理验证码输入
-        
+
         Args:
             login_window: 登录窗口
             login_window_handle: 登录窗口句柄
-            
+
         Returns:
             int: 验证码处理结果
                 0: 成功
@@ -113,22 +113,31 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
             # 获取所有控件的详细信息
             win_children = login_window.children()
             # 过滤出Edit控件
-            edit_ctrls = [child for child in win_children if child.class_name() == 'Edit' and child.is_visible() and child.is_enabled()]
-            
+            edit_ctrls = [
+                child
+                for child in win_children
+                if child.class_name() == "Edit"
+                and child.is_visible()
+                and child.is_enabled()
+            ]
+
             logger.debug(f"找到的Edit控件数量: {len(edit_ctrls)}")
 
-            if (len(edit_ctrls) == 2):
+            if len(edit_ctrls) == 2:
                 logger.info("没有找到验证码控件，跳过验证码输入")
                 return 1  # 没有验证码控件
-            
+
             # 找到用户名和密码之外的Edit控件
             verify_ctrl = None
             for ctrl in edit_ctrls:
-                if ctrl.control_id() not in [login_window.Edit1.control_id(), login_window.Edit2.control_id()]:
+                if ctrl.control_id() not in [
+                    login_window.Edit1.control_id(),
+                    login_window.Edit2.control_id(),
+                ]:
                     logger.debug("控件ID:%s", ctrl.control_id())
                     verify_ctrl = ctrl
                     break
-            
+
             if verify_ctrl is None:
                 logger.info("没有找到验证码控件，跳过验证码输入")
                 return 1  # 没有验证码控件
@@ -139,30 +148,30 @@ class THS519ClientTrader(clienttrader.BaseLoginClientTrader):
                 try:
                     verify_ctrl.set_focus()
                     logger.info("清除上一次输入的验证码")
-                    verify_ctrl.type_keys('^a{BACKSPACE}')
+                    verify_ctrl.type_keys("^a{BACKSPACE}")
                     time.sleep(1)
-                    code = self._handle_verify_code(handle=login_window_handle)
+                    code = self._handle_verify_code()
                     verify_ctrl.type_keys(code)
                     logger.info("解析验证码解析成功，并输入到Edit框：%s", code)
                     time.sleep(1)
                     # 点击右侧的确定按钮来登录
-                    self._app.window(handle=login_window_handle)["登录"].click()
+                    self._app.top_window()["登录"].click()
                     # 等待登录窗口消失
-                    self._app.window(handle=login_window_handle).wait_not("exists", 30)
+                    self._app.top_window().wait_not("exists", 30)
                     return 0  # 成功
                 except Exception:
                     time.sleep(1)
                     logger.debug("登录失败，再一次尝试")
                     retry += 1
-            
+
             logger.error("验证码输入多次失败")
             return 2  # 验证码输入失败
         except Exception:
             logger.info("验证码处理发生异常")
             return 2  # 验证码处理异常
 
-    def _handle_verify_code(self, handle):
-        control = self._app.window(handle=handle).window(control_id=0x5db)
+    def _handle_verify_code(self):
+        control = self._app.top_window().window(control_id=0x5DB)
         control.click()
         time.sleep(0.2)
         file_path = tempfile.mktemp() + ".jpg"
